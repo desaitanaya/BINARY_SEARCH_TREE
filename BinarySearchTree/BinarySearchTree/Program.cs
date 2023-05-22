9ec31e708a0e4f87d903cff9c975a7c6320c6bd6
Tanaya <desaitanaya17@gmail.com>
2023-05-22 20:58:51 +0530

    [Tanaya] Add : C# Program to implement add method in Binary Search Tree.

---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> node = new Node<int>();
+
+            node.InsertNode(10);
+            node.InsertNode(20);
+            node.InsertNode(30);
+            node.InsertNode(2);
+            node.InsertNode(45);
+            node.InsertNode(83);
+            node.Display();
+        }
+    }
+}
